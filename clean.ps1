2de7e371d2ba6ff81c5a17339847c99be87e4724
Rob Ahnemann <rob.ahnemann@gmail.com>
2022-07-27 11:49:15 -0500

    Initial commit

---
+++ clean.ps1
@@ -0,0 +1,6 @@
+# Clean data folders
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\docker\data\mssql") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\docker\data\solr") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\docker\data\cm") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\docker\data\cd") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+Get-ChildItem -Path (Join-Path $PSScriptRoot "\docker\deploy\website") -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
