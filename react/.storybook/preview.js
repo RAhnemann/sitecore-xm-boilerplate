64f7aaaf635a0ab8d9fa95916bc556962f57508e
Rob Ahnemann <rob.ahnemann@gmail.com>
2022-09-20 16:23:41 -0500

    Small Tweaks

--- react/.storybook/preview.js
+++ react/.storybook/preview.js
@@ -1,4 +1,4 @@
-import { MockedProvider } from '@apollo/react-testing';
+import { MockedProvider } from '@apollo/client/testing';
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import '../src/scss/global.scss';
 
